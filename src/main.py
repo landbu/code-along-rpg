61e04463c869fda6de10215132bfcdc42712867a
Landbu <adam.landbu@gmail.com>
2022-02-11 13:35:13 +0100

    added goblin and fight function

--- src/main.py
+++ src/main.py
@@ -1,10 +1,32 @@
-from resources import Character
+from resources import Character, Goblin
+import random
+
+def fight(fighter : Character, enemies : list):
+
+    while not len(enemies) == 0:
+        fighter_target = random.choice(enemies)
+        fighter_target.take_damage(fighter.attack())
+        if fighter_target.get_health() == 0:
+            print("Target has died")
+            enemies.remove(fighter_target)
+            if len(enemies) == 0: break
+        
+
+    print(f"Fight is over, {fighter.name} won!")
+
 
 def main():
+    enemies = []
+
     nick = Character("Nick", 15, 3, 1)
     emy = Character("Emy", 20, 6, 5)
 
-    print(nick)
-    print(emy)
+    print(nick,"\n")
+    print(emy,"\n")
+
+    enemies.append(Goblin(1))
+    print(enemies[0])
+
+    fight(emy, enemies)
 
 main()
--- src/resources.py
+++ src/resources.py
@@ -9,3 +9,35 @@ class Character:
 
     def __str__(self) -> str:
         return f"Name : {self.name}\nHealth: {self.health}\nDamage: {self.damage}\nArmor: {self.armor}"
+
+    def take_damage(self, dmg):
+            actual_damage = dmg - self.armor
+            if (self.health - actual_damage) < self.health: self.health = 0
+            else: self.health -= actual_damage
+
+    def attack(self):
+        return self.damage
+    
+class Goblin:
+
+    def __init__(self, id):
+        self.id = id
+        self.health = 10
+        self.damage = 3
+        self.armor = 2
+
+    def take_damage(self, dmg):
+        actual_damage = dmg - self.armor
+        if (self.health - actual_damage) < self.health: self.health = 0
+        else: self.health -= actual_damage
+
+
+
+    def __str__(self) -> str:
+        return f"Goblin Id : {self.id}\nHealth: {self.health}\nDamage: {self.damage}\nArmor: {self.armor}"
+    
+    def attack(self):
+        return self.damage
+
+    def get_health(self):
+        return self.health
